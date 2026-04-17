fb1dcf90c569b76b68520544e7a3c61cd010385e
Omkar <omthkd7@gmail.com>
2026-04-17 20:48:35 +0530

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+// Top-Down Shooter Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<SDL.h>
+#include<SDL_ttf.h>
+#include <iostream>
+#define SDL_MAIN_HANDLED
+int main()
+{
+    std::cout << "Hello World!\n";
+}
